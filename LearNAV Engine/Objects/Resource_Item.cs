0d86285ace2f40125688f2e911225f4fbec3a860
Jureen Sese <sesejureenblog@gmail.com>
2020-09-22 12:07:52 +0800

    Updated overall structure. Preparation for Initial Release.

--- LearNAV Engine/Objects/Resource_Item.cs
+++ LearNAV Engine/Objects/Resource_Item.cs
@@ -18,17 +18,20 @@ namespace LearNAV_Engine
       public string Resource_Loc;
       public string[] Resource_Tags;
 
-      public Resource_Item(string r_id, string r_name, string r_auth, string r_loc, string[] r_tags)
+      public Resource_Item(int r_id, string r_name, string r_auth, string r_loc, string[] r_tags)
       {
           Resource_ID = r_id;
           Resource_Name = r_name;
-          Resource_Author = r_author;
+          Resource_Author = r_auth;
           Resource_Loc = r_loc;
-          Resource_Tags[] = r_tags[];
+          Resource_Tags = r_tags;
 
       }
 
       public static string Object_Info
+      {
+
+      }
 
   
     }
